995b083ddcda064b255d3d10671cb75d4bb0b652
GKEnev24 <GKEnev24@codingburgas.bg>
2025-11-25 07:57:25 +0200

    Add temporary background

--- error404/error404/error404.cpp
+++ error404/error404/error404.cpp
@@ -5,13 +5,17 @@ int main() {
 
 	SetTargetFPS(60);
 
+	Texture2D backgroud = LoadTexture("../assets/images/menu-background.png");
+
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 
 		ClearBackground(WHITE);
+		DrawTexture(backgroud, 0, 0, WHITE);
 
 		EndDrawing();
 	}
 
+	UnloadTexture(backgroud);
 	CloseWindow();
 }
