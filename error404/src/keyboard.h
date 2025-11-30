4320d853671ee82641b20e029ae70f3984e8a832
zigeorgakiev24 <zigeorgakiev24@codingburgas.bg>
2025-11-30 23:50:59 +0200

    Add keyboard texture

---
+++ error404/src/keyboard.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "button.hpp"
+
+void keyboard() {
+    
+
+    
+}
--- error404/src/main.cpp
+++ error404/src/main.cpp
@@ -1,5 +1,6 @@
 #include "button.hpp"
 #include "word-select.h"
+#include "keyboard.h"
 
 #include <raylib.h>
 #include <iostream>
@@ -10,7 +11,6 @@
 
 
 using namespace std;
-
 
 typedef enum Screen {
     MAINMENU,
@@ -26,23 +26,64 @@ typedef enum Screen {
 Screen currentScreen = MAINMENU;
 
 int main()
-{
+{   
     srand(time(NULL));
 
     InitWindow(800, 600, "Raylib Buttons Tutorial");
 
+    SetExitKey(KEY_NULL);
+
     SetTargetFPS(60);
 
-    Texture2D background = LoadTexture("../assets/images/menu-background.png");
+    Texture2D background = LoadTexture("../assets/images/background.png");
+    Texture2D logo = LoadTexture("../assets/images/logo.png");
 
-    Button startButton("../assets/images/start-button.png", { 320, 300 }, 0.40f);
-    Button exitButton("../assets/images/exit-button.png", { 320, 370 }, 0.40f);
+    
 
-    Button hardButton("../assets/images/start-button.png", { 320,  200}, 0.40f);
-    Button mediumButton("../assets/images/start-button.png", { 320, 270 }, 0.40f);
-    Button easyButton("../assets/images/start-button.png", { 320, 340 }, 0.40f);
+    Button startButton("../assets/buttons/start-button.png", { 335, 230 }, 0.60f); 
+    Button exitButton("../assets/buttons/exit-button.png", { 343, 500 }, 0.60f);
 
-    Button backButton("../assets/images/exit-button.png", { 320, 450 }, 0.40f);
+    Button hardButton("../assets/buttons/hard-button.png", { 130,  270}, 0.15f); 
+    Button mediumButton("../assets/buttons/medium-button.png", { 300, 270 }, 0.21f);
+    Button easyButton("../assets/buttons/easy-button.png", { 530, 270 }, 0.15f);
+
+    Button backButton("../assets/buttons/back-button.png", { 320, 450 }, 0.15f);
+
+    Button resumeButton("../assets/buttons/resume-button.png", { 320, 200}, 0.40f);
+
+    Button rulesButton("../assets/buttons/rules-button.png", { 315, 320 }, 0.60f);
+    Button creditsButton("../assets/buttons/credits-button.png", { 290, 410 }, 0.60f);
+
+    Button qKeyButton("../assets/buttons/q-key-button.png", { 30, 300 }, 0.80f);
+    Button wKeyButton("../assets/buttons/w-key-button.png", { 105, 300 }, 0.80f);
+    Button eKeyButton("../assets/buttons/e-key-button.png", { 190, 300 }, 0.80f);
+    Button rKeyButton("../assets/buttons/r-key-button.png", { 255, 300 }, 0.80f);
+    Button tKeyButton("../assets/buttons/t-key-button.png", { 330, 300 }, 0.80f);
+    Button yKeyButton("../assets/buttons/y-key-button.png", { 405, 300 }, 0.80f);
+    Button uKeyButton("../assets/buttons/u-key-button.png", { 480, 300 }, 0.80f);
+    Button iKeyButton("../assets/buttons/i-key-button.png", { 570, 300 }, 0.80f);
+    Button oKeyButton("../assets/buttons/o-key-button.png", { 630, 300 }, 0.80f);
+    Button pKeyButton("../assets/buttons/p-key-button.png", { 705, 300 }, 0.80f);
+
+    float offsetA = 35;
+    Button aKeyButton("../assets/buttons/a-key-button.png", { 30 + offsetA, 400 }, 0.80f);
+    Button sKeyButton("../assets/buttons/s-key-button.png", { 105 + offsetA, 400 }, 0.80f);
+    Button dKeyButton("../assets/buttons/d-key-button.png", { 180 + offsetA, 400 }, 0.80f);
+    Button fKeyButton("../assets/buttons/f-key-button.png", { 255 + offsetA, 400 }, 0.80f);
+    Button gKeyButton("../assets/buttons/g-key-button.png", { 330 + offsetA, 400 }, 0.80f);
+    Button hKeyButton("../assets/buttons/h-key-button.png", { 405 + offsetA, 400 }, 0.80f);
+    Button jKeyButton("../assets/buttons/j-key-button.png", { 480 + offsetA, 400 }, 0.80f);
+    Button kKeyButton("../assets/buttons/k-key-button.png", { 555 + offsetA, 400 }, 0.80f);
+    Button lKeyButton("../assets/buttons/l-key-button.png", { 630 + offsetA, 400 }, 0.80f);
+
+    float offsetZ = 125;
+    Button zKeyButton("../assets/buttons/z-key-button.png", { 30 + offsetZ, 500 }, 0.80f);
+    Button xKeyButton("../assets/buttons/x-key-button.png", { 105 + offsetZ, 500 }, 0.80f);
+    Button cKeyButton("../assets/buttons/c-key-button.png", { 180 + offsetZ, 500 }, 0.80f);
+    Button vKeyButton("../assets/buttons/v-key-button.png", { 255 + offsetZ, 500 }, 0.80f);
+    Button bKeyButton("../assets/buttons/b-key-button.png", { 330 + offsetZ, 500 }, 0.80f);
+    Button nKeyButton("../assets/buttons/n-key-button.png", { 405 + offsetZ, 500 }, 0.80f);
+    Button mKeyButton("../assets/buttons/m-key-button.png", { 480 + offsetZ, 500 }, 0.80f);
 
     int gameDifculty = 0;
 
@@ -64,7 +105,11 @@ int main()
         {
             case MAINMENU:
             {
+                DrawTexture(logo, 200, 60, WHITE);
+                
                 startButton.Draw();
+                rulesButton.Draw();
+                creditsButton.Draw();
                 exitButton.Draw();
                 
                 if (startButton.isPressed(mousePosition, mousePressed)) {
@@ -79,6 +124,8 @@ int main()
             
             case GAMEDIFICULTY:
             {
+                DrawText("Choose game dificulty", 140, 150, 50, WHITE);
+                
                 hardButton.Draw();
                 mediumButton.Draw();
                 easyButton.Draw();
@@ -127,6 +174,8 @@ int main()
 
                     cout << endl;
 
+
+
                     currentScreen = GAME;
                 }
 
@@ -137,12 +186,126 @@ int main()
             
             case GAME:
             {
+                keyboard();
+
+                qKeyButton.Draw();
+                wKeyButton.Draw();
+                eKeyButton.Draw();
+                rKeyButton.Draw();
+                tKeyButton.Draw();
+                yKeyButton.Draw();
+                uKeyButton.Draw();
+                iKeyButton.Draw();
+                oKeyButton.Draw();
+                pKeyButton.Draw();
+                aKeyButton.Draw();
+                sKeyButton.Draw();
+                dKeyButton.Draw();
+                fKeyButton.Draw();
+                gKeyButton.Draw();
+                hKeyButton.Draw();
+                jKeyButton.Draw();
+                kKeyButton.Draw();
+                lKeyButton.Draw();
+                zKeyButton.Draw();
+                xKeyButton.Draw();
+                cKeyButton.Draw();
+                vKeyButton.Draw();
+                bKeyButton.Draw();
+                nKeyButton.Draw();
+                mKeyButton.Draw();
+
+                if (qKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_Q)) {
+                    
+                }
+                else if(wKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_W)) {
+
+                }
+                else if (eKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_E)) {
+
+                }
+                else if (rKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_R)) {
+
+                }
+                else if (tKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_T)) {
+
+                }
+                else if (yKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_Y)) {
+
+                }
+                else if (uKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_U)) {
+
+                }
+                else if (iKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_I)) {
+
+                }
+                else if (oKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_O)) {
+
+                }
+                else if (pKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_P)) {
+
+                }
+                else if (aKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_A)) {
+
+                }
+                else if (sKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_S)) {
+
+                }
+                else if (dKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_D)) {
+
+                }
+                else if (fKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_F)) {
+
+                }
+                else if (gKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_G)) {
+
+                }
+                else if (hKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_H)) {
+
+                }
+                else if (jKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_J)) {
+
+                }
+                else if (kKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_K)) {
+
+                }
+                else if (lKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_L)) {
+
+                }
+                else if (zKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_Z)) {
+
+                }
+                else if (xKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_X)) {
+
+                }
+                else if (cKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_C)) {
+
+                }
+                else if (vKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_V)) {
+
+                }
+                else if (bKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_B)) {
+
+                }
+                else if (nKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_N)) {
+
+                }
+                else if (mKeyButton.isPressed(mousePosition, mousePressed) or IsKeyDown(KEY_M)) {
+
+                }
                 
+                if (IsKeyDown(KEY_ESCAPE)) {
+                    currentScreen = GAMEMENU;
+                }
             }break;
             
             case GAMEMENU:
             {
-                
+                resumeButton.Draw();
+
+                if (resumeButton.isPressed(mousePosition, mousePressed)) {
+                    currentScreen = GAME;
+                }
             }break;
             
             case WON:
